7e112a4a25f231c02a3f0d0084e73c1e334a7662
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2014-01-29 13:37:29 -0500

    More changes

--- XMLParsing/XMLParsing/MDF1ViewController.m
+++ XMLParsing/XMLParsing/MDF1ViewController.m
@@ -16,9 +16,32 @@
 
 - (void)viewDidLoad
 {
+    NSData *theXMLData = [self GetFileDataFromFile:@""]; //Still have no file to put in here
+    
+    NSXMLParser *theParse = [[NSXMLParser alloc] initWithData:theXMLData];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+
+-(NSData*)GetFileDataFromFile:(NSString*)filename
+{
+    NSString *theFilePath = nil;
+    
+    NSFileManager *getFileManager = [NSFileManager defaultManager]; //Create the file manager
+    
+    NSArray *thePath = NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES); //Get the path to the document directory
+    
+    NSString *theDocumentDirectory = [thePath objectAtIndex: 0];
+    
+    theFilePath = [theDocumentDirectory stringByAppendingPathComponent:filename]; //Create the the fullpath to the data file
+    
+    if ([getFileManager fileExistsAtPath:theFilePath]) //Does it exist?
+    {
+        return [NSData dataWithContentsOfFile:theFilePath]; //returns back the NSData for the file
+    }
+    return nil;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
